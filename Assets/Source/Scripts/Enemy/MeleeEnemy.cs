4e69d291a9047e4ccb79efbbed961151d50d67b6
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-29 16:49:14 -0800

    added Enemy subtypes

---
+++ Assets/Source/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeEnemy : Enemy {
+
+	//Melee enemy runs up to the camera, stops, and goes into attack mode where it swings
+}
---
+++ Assets/Source/Scripts/Enemy/RangedEnemy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangedEnemy : Enemy {
+
+	//Ranged enemies come into the scene, stop, and begin firing at player
+
+
+
+
+}
