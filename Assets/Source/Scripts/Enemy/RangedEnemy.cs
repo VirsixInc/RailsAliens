6d7e27de04eacae545741bf1a769f91321eee13a
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-29 17:39:41 -0800

    More on Enemy Types

--- Assets/Source/Scripts/Enemy/RangedEnemy.cs
+++ Assets/Source/Scripts/Enemy/RangedEnemy.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class RangedEnemy : Enemy {
 
 	//Ranged enemies come into the scene, stop, and begin firing at player
+	public ProjectileTypes projectileType;
 
+	public float attackFrequency;
 
+	public override void Attack() {
+	}
 
 
 }
--- Assets/Source/Scripts/GameControllers/FirstPersonController.cs
+++ Assets/Source/Scripts/GameControllers/FirstPersonController.cs
@@ -4,25 +4,11 @@ using System.Collections;
 public class FirstPersonController : MonoBehaviour {
 
 	public float movementSpeed = 5.0f;
-	public float upDownRange = 60.0f;
-	public float verticalRotation = 0.0f;
 
-	// Update is called once per frame
 	void Update () {
 
-//		//rotation
-//		float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity; //retrieve lateral mouse movement
-//		transform.Rotate (0, rotLeftRight, 0);
-//
-//		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity; //retrieve vertical mouse movement
-//		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange); //do not let vertical movement beyond range
-//
-//		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0); //move the camera (instead of controller) vertically 
-
-		//movement
-
-		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed; //request speed from input which is already mapped to asdw
-		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed; //request speed from input which is already mapped to asdw
+		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
 
 		Vector3 speed = new Vector3 (sideSpeed, 0, forwardSpeed); //create a direction vector from forwardSpeed
 
